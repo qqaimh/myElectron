81ae9eec7786d914e97b606fce9160ff7d48f069
qqaimh <294050076@qq.com>
2021-11-28 14:14:51 +0800

    my myElectron

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+const url = require("url")
+const path = require("path")
+
+let mainWindow
+
+const winURL = `file://${__dirname}/dist/myElectron/index.html`
+const devURL = `http://localhost:4200/`
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation: false,
+            preload: __dirname + '/preload.js'
+        }
+    })
+
+    mainWindow.loadURL(winURL);
+    // 开启渲染进程中的调试模式
+    // mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+console.log(app);
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+    if (mainWindow === null) createWindow()
+})
+
+ipcMain.on('clickMessage',(event, arg) => {
+    console.log(778899)
+    mainWindow.webContents.send('clickMessageBack', '来自于主线程' );
+})
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+
+import { IpcRenderer } from 'electron';
+
+declare global {
+  interface Window {
+    ipcRenderer: IpcRenderer
+  }
+}
+
+export const { ipcRenderer } = window;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myElectron';
+  clickFrom = '';
+  
+  constructor(public cdr: ChangeDetectorRef) {
+   
+  }
+
+  ngOnInit(): void {
+    ipcRenderer.on('clickMessageBack', (event, message) => {
+      this.clickFrom = message;
+      this.cdr.detectChanges();
+    })
+  }
+
+  directOpenModal() {
+    this.clickFrom = '直接点击'
+  }
+
+  indirectOpenModal() {
+    ipcRenderer.send("clickMessage", {type:'indirectClick'});
+  }
+
 }
